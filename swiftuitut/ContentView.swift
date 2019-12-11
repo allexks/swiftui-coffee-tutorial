76e03b5dc975b33e2de2d6b9e55452b2a97e1971
Alexander Ignatov <ani1802@abv.bg>
2019-12-11 12:45:13 +0200

    Initial Commit

---
+++ swiftuitut/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftuitut
+//
+//  Created by Aleksandar Ignatov on 11.12.19.
+//  Copyright © 2019 MentorMate. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
